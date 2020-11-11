146bfad75e2d731180bf259b518d28a66bc9cf9b
jobairRafi96 <jubairrafi96960@gmail.com>
2020-11-11 23:27:40 +0600

    employer update and delete

--- ATP3_lab_performance/models/employerModel.js
+++ ATP3_lab_performance/models/employerModel.js
@@ -1,3 +1,4 @@
+
 const db = require("./db");
 
 module.exports = {
@@ -30,5 +31,32 @@ module.exports = {
               callback(status)
           })
           
-    }
+    },
+
+    getByID : function(id,callback){
+        var sql = "select * from user where id='"+id+"'";
+        db.getResults(sql,results=>{
+            callback(results)
+        })
+    },
+
+    update: function(id,employerEditedInfo,callback){
+      var sql = "update user set employerName = '"+employerEditedInfo.employerName+"',companyName = '"+employerEditedInfo.companyName+"',contact = '"+employerEditedInfo.contact+"',username = '"+employerEditedInfo.username+"',password = '"+employerEditedInfo.password+"' where id='"+id+"'";
+        
+      db.execute(sql,status=>{
+          callback(status)
+      })
+    },
+
+
+    delete: function(id,callback){
+       var sql = "delete from user where id='"+id+"'";
+       db.execute(sql,status=>{
+        callback(status)
+    })
+ }
+
+    
+
+    
 }
